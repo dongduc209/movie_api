f974384769e26677e3ec1eb0a0eb95e0b35fb5f9
dongduc209 <dongduc2091995@gmail.com>
2023-06-17 20:59:16 -0400

    Initial update

--- index.js
+++ index.js
@@ -1,80 +1,222 @@
-const express = require('express'),
-    morgan = require('morgan');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const uuid = require('uuid');
+const morgan = require('morgan');
 const fs = require('fs');
 const path = require('path');
+const { title } = require('process');
 
-const app = express();
+app.use(bodyParser.json());
 
-let topMovies = [
+let users = [
   {
-    title: 'Black Panther: Wakanda Forever',
-    director: 'Ryan Coogler'
+    id: 1,
+    name: "Duc Huynh",
+    favoriteMovies: []
   },
   {
-    title: 'Lord of the Rings',
-    director: 'J.R.R. Tolkien'
+    id: 2,
+    name: "Thao Nguyen",
+    favoriteMovies: []
   },
   {
-    title: 'Twilight',
-    director: 'Stephanie Meyer'
+    id: 3,
+    name: "Lucas Huynh",
+    favoriteMovies: []
   },
   {
-    title: 'Lord of the Rings',
-    director: 'J.R.R. Tolkien'
-  },
-  {
-    title: 'Guardians of the Galaxy',
-    director: 'James Gunn'
-  },
-  {
-    title: 'Spider-Man: Homecoming',
-    director: 'Jon Watts'
-  },
-  {
-    title: 'The Avengers',
-    director: 'Joss Whedon'
-  },
-  {
-    title: 'Captain America: Civil War',
-    director: 'Anthony Russo, Joe Russo'
-  },
-  {
-    title: 'Black Panther',
-    director: 'Ryan Coogler'
-  },
-  {
-    title: 'Doctor Strange in the Multiverse of Madness',
-    director: 'Sam Raimi'
+    id: 4,
+    name: "Somebody else",
+    favoriteMovies: []
   }
 ];
 
-app.use(morgan('common'));
+let movies = [
+  {
+    "Title":"Black Panther",
+    "Description":"T'Challa, heir to the hidden but advanced kingdom of Wakanda, must step forward to lead his people into a new future and must confront a challenger from his country's past.",
+    "Genre": {
+      "Name":"Action",
+      "Description":""},
+    "Director":{
+    "Name":"Ryan Coogler",
+    "Bio":"Ryan Kyle Coogler is an African-American filmmaker and producer who is from Oakland, California. He is known for directing the Black Panther film series, Creed, a Rocky spin-off and Fruitvale Station. He frequently casts Michael B. Jordan in his works. He produced the Creed sequels, Judas and the Black Messiah and Space Jam: A New Legacy.",
+    "Birth": 1986
+    },
+    "Feature":false
+  },
+  {
+    "Title":"Kung Fu Panda",
+    "Description":"To everyone's surprise, including his own, Po, an overweight, clumsy panda, is chosen as protector of the Valley of Peace. His suitability will soon be tested as the valley's arch-enemy is on his way.",
+    "Genre": {
+      "Name":"Animation",
+      "Description":""
+    },
+    "Director":{
+    "Name":"Mark Osbornen",
+    "Bio":"Mark Randolph Osborne is an American film director, writer, producer and animator from Trenton, New Jersey who is known for co-directing the Oscar nominated Kung Fu Panda (2008) and The SpongeBob SquarePants Movie (2004).",
+    "Birth":1970
+    },
+    "Feature":false
+  },{
+    "Title":"Big Hero 6",
+    "Description":"A special bond develops between plus-sized inflatable robot Baymax and prodigy Hiro Hamada, who together team up with a group of friends to form a band of high-tech heroes.",
+    "Genre": {
+      "Name":"Animation",
+      "Description":""
+    },
+    "Director":{
+    "Name":"Don Hall",
+    "Bio":"Don Hall was born on March 8, 1969 in Glenwood, Iowa, USA. He is a writer and director, known for Big Hero 6 (2014), Meet the Robinsons (2007) and Raya and the Last Dragon (2021).",
+    "Birth":1969
+    },
+    "Feature":false
+  },  
+];
 
-app.get('/', (req, res) => {
-  res.send('Welcome to my Movie Page!');
-});
+//READ
+app.get('/movies', (req,res) => {
+  res.status(200).json(movies);
+})
 
-app.get('/documentation', (req, res) => {                  
-  res.sendFile('public/documentation.html', { root: __dirname });
-});
+//READ
+app.get('/movies/:title', (req,res) => {
+  const { title } = req.params;
+  const movie = movies.find( movie => movie.Title === title );
 
-app.get('/movies', (req, res) => {
-  res.json(topMovies);
-});
+  if(movie) {
+    res.status(200).json(movie);
+  } else {
+    res.status(400).send('no such movie')
+  }
+})
 
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-  });
+//READ
+app.get('/movies/genre/:genreName', (req,res) => {
+  const { genreName } = req.params;
+  const genre = movies.find( movie => movie.Genre.Name === genreName ).Genre;
 
-const accessLogStream = fs.createWriteStream(
-    path.join(__dirname, 'log.txt'), {flags: 'a'});
+  if(genre) {
+    res.status(200).json(genre);
+  } else {
+    res.status(400).send('no such genre')
+  }
+})
 
-app.use(morgan('combined', {stream: accessLogStream}));
+//READ
+app.get('/movies/directors/:directorName', (req,res) => {
+  const { directorName } = req.params;
+  const director = movies.find( movie => movie.Director.Name === directorName ).Director;
+
+  if(director) {
+    res.status(200).json(director);
+  } else {
+    res.status(400).send('no such director')
+  }
+})
+//CREATE
+app.post('/users', (req, res) => {
+  const newUser = req.body;
+
+  if(newUser.name) {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).json(newUser)
+  } else {
+    res.status(400).send('user need name')
+  }
+})
+
+//UPDATE
+app.put('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const updateUser = req.body;
+
+  let user = users.find( user => user.id == id );
+
+  if(user) {
+    user.name = updateUser.name;
+    res.status(200).json(user);
+  } else {
+    res.status(400).send('no such user')
+  }
   
-app.get('/secreturl', (req, res) => {
-    res.send('This is a secret url with super top-secret content.');
-  });
+})
+
+//CREATE
+app.post('/users/:id/:movieTitle', (req, res) => {
+  const { id, movieTitle } = req.params;
+
+  let user = users.find( user => user.id == id );
+
+  if(user) {
+    user.favoriteMovies.push(movieTitle);
+    res.status(200).json(user);
+  } else {
+    res.status(400).send('no such movie')
+  }
+  
+})
+
+//DELETE
+app.delete('/users/:id/:movieTitle', (req, res) => {
+  const { id, movieTitle } = req.params;
+
+  let user = users.find( user => user.id == id );
+
+  if(user) {
+    user.favoriteMovies = user.favoriteMovies.filter( title => title !== movieTitle);
+    res.status(200).send(`${movieTitle} has been removed from user ${id}'s array`);
+  } else {
+    res.status(400).send('no such movie')
+  }
+  
+})
+
+//DELETE
+app.delete('/users/:id', (req, res) => {
+  const { id } = req.params;
+
+  let user = users.find( user => user.id == id );
+
+  if(user) {
+    users = users.filter( user => user.id != id);
+    res.status(200).send(`user ${id} has been deleted!`);
+  } else {
+    res.status(400).send('no such user')
+  }
+  
+})
+
+
+// app.use(morgan('common'));
+
+// app.get('/', (req, res) => {
+//   res.send('Welcome to my Movie Page!');
+// });
+
+// app.get('/documentation', (req, res) => {                  
+//   res.sendFile('public/documentation.html', { root: __dirname });
+// });
+
+// app.get('/movies', (req, res) => {
+//   res.json(topMovies);
+// });
+
+// app.use((err, req, res, next) => {
+//     console.error(err.stack);
+//     res.status(500).send('Something broke!');
+//   });
+
+// const accessLogStream = fs.createWriteStream(
+//     path.join(__dirname, 'log.txt'), {flags: 'a'});
+
+// app.use(morgan('combined', {stream: accessLogStream}));
+  
+// app.get('/secreturl', (req, res) => {
+//     res.send('This is a secret url with super top-secret content.');
+//   });
   
 app.listen(8080, () => {
     console.log('Your app is listening on port 8080.');
