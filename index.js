daa8a2d343072a9353fd217589d9f8286e2efa42
dongduc209 <dongduc2091995@gmail.com>
2023-07-09 21:28:20 -0400

    updated type of data

--- index.js
+++ index.js
@@ -1,223 +1,195 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const uuid = require('uuid');
+const express = require('express'),
+      bodyParser = require('body-parser'),
+      uuid = require('uuid');
+
 const morgan = require('morgan');
+const app = express();
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+
+const Movies = Models.Movie;
+const Users = Models.User;
+const Genres = Models.Genre;
+const Directors = Models.Director;
+
+mongoose.connect('mongodb://127.0.0.1:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
+
 const fs = require('fs');
 const path = require('path');
 const { title } = require('process');
 
 app.use(bodyParser.json());
 
-let users = [
-  {
-    id: 1,
-    name: "Duc Huynh",
-    favoriteMovies: []
-  },
-  {
-    id: 2,
-    name: "Thao Nguyen",
-    favoriteMovies: []
-  },
-  {
-    id: 3,
-    name: "Lucas Huynh",
-    favoriteMovies: []
-  },
-  {
-    id: 4,
-    name: "Somebody else",
-    favoriteMovies: []
-  }
-];
+app.get("/", (req, res) => {
+  res.send("Welcome to myFlix!");
+});
 
-let movies = [
-  {
-    "Title":"Black Panther",
-    "Description":"T'Challa, heir to the hidden but advanced kingdom of Wakanda, must step forward to lead his people into a new future and must confront a challenger from his country's past.",
-    "Genre": {
-      "Name":"Action",
-      "Description":""},
-    "Director":{
-    "Name":"Ryan Coogler",
-    "Bio":"Ryan Kyle Coogler is an African-American filmmaker and producer who is from Oakland, California. He is known for directing the Black Panther film series, Creed, a Rocky spin-off and Fruitvale Station. He frequently casts Michael B. Jordan in his works. He produced the Creed sequels, Judas and the Black Messiah and Space Jam: A New Legacy.",
-    "Birth": 1986
-    },
-    "Feature":false
-  },
-  {
-    "Title":"Kung Fu Panda",
-    "Description":"To everyone's surprise, including his own, Po, an overweight, clumsy panda, is chosen as protector of the Valley of Peace. His suitability will soon be tested as the valley's arch-enemy is on his way.",
-    "Genre": {
-      "Name":"Animation",
-      "Description":""
-    },
-    "Director":{
-    "Name":"Mark Osbornen",
-    "Bio":"Mark Randolph Osborne is an American film director, writer, producer and animator from Trenton, New Jersey who is known for co-directing the Oscar nominated Kung Fu Panda (2008) and The SpongeBob SquarePants Movie (2004).",
-    "Birth":1970
-    },
-    "Feature":false
-  },{
-    "Title":"Big Hero 6",
-    "Description":"A special bond develops between plus-sized inflatable robot Baymax and prodigy Hiro Hamada, who together team up with a group of friends to form a band of high-tech heroes.",
-    "Genre": {
-      "Name":"Animation",
-      "Description":""
-    },
-    "Director":{
-    "Name":"Don Hall",
-    "Bio":"Don Hall was born on March 8, 1969 in Glenwood, Iowa, USA. He is a writer and director, known for Big Hero 6 (2014), Meet the Robinsons (2007) and Raya and the Last Dragon (2021).",
-    "Birth":1969
-    },
-    "Feature":false
-  },  
-];
+//Return a list of ALL movies to the user
+app.get('/movies', (req, res) => {
+  Movies.find()
+  .then((movies) => {
+    res.status(201).json(movies);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send("Error: " + err);
+  });
+});
 
-//READ
-app.get('/movies', (req,res) => {
-  res.status(200).json(movies);
-})
+//Return data about a single movie by title to the user
+app.get('/movies/:Title', (req, res) => {
+  Movies.findOne({ Title: req.params.Title })
+  .then((movie) => {
+    res.json(movie);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send("Error: " + err);
+  });
+});
 
-//READ
-app.get('/movies/:title', (req,res) => {
-  const { title } = req.params;
-  const movie = movies.find( movie => movie.Title === title );
+//Return data about a genre by name/title
+app.get('/movies/Genre/:genreName', (req, res) => {
+  Movies.findOne({ "Genre.Name" : req.params.genreName })
+  .then((movie) => {
+    res.json(movie.Genre);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send("Error: " + err);
+  });
+});
 
-  if(movie) {
-    res.status(200).json(movie);
-  } else {
-    res.status(400).send('no such movie')
-  }
-})
+//Return data about a director (bio, birth year, death year) by name
+app.get('/movies/Director/:directorName', (req, res) => {
+  Movies.findOne({ "Director.Name" : req.params.directorName })
+  .then((movie) => {
+    res.json(movie.Director);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send("Error: " + err);
+  });
+});
 
-//READ
-app.get('/movies/genre/:genreName', (req,res) => {
-  const { genreName } = req.params;
-  const genre = movies.find( movie => movie.Genre.Name === genreName ).Genre;
+//Return a list of ALL users
+app.get('/users', (req, res) => {
+  Users.find()
+  .then((users) => {
+    res.status(201).json(users);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send("Error: " + err);
+  });
+});
 
-  if(genre) {
-    res.status(200).json(genre);
-  } else {
-    res.status(400).send('no such genre')
-  }
-})
+//Return data about user by Username
+app.get('/users/:UserName', (req, res) => {
+  Users.findOne({ UserName: req.params.UserName })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
 
-//READ
-app.get('/movies/directors/:directorName', (req,res) => {
-  const { directorName } = req.params;
-  const director = movies.find( movie => movie.Director.Name === directorName ).Director;
-
-  if(director) {
-    res.status(200).json(director);
-  } else {
-    res.status(400).send('no such director')
-  }
-})
-//CREATE
+//Allow new user to register
 app.post('/users', (req, res) => {
-  const newUser = req.body;
+  Users.findOne({ UserName: req.body.UserName })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.UserName + 'already exists');
+      } else {
+        Users
+          .create({
+            UserName: req.body.UserName,
+            Password: req.body.Password,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+          })
+          .then((user) =>{res.status(201).json(user) })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send('Error: ' + error);
+        })
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
+});
 
-  if(newUser.name) {
-    newUser.id = uuid.v4();
-    users.push(newUser);
-    res.status(201).json(newUser)
-  } else {
-    res.status(400).send('user need name')
-  }
-})
+//Allow user to update their info
+app.put('/users/:UserName', (req, res) => {
+  Users.findOneAndUpdate({ UserName: req.params.UserName }, { $set:
+    {
+      UserName: req.body.UserName,
+      Password: req.body.Password,
+      Email: req.body.Email,
+      Birthday: req.body.Birthday
+    }
+  },
+  { new: true })
+  .then(updatedUser => {
+    res.json(updatedUser);
+  })
+  .catch(err => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });
+});
 
-//UPDATE
-app.put('/users/:id', (req, res) => {
-  const { id } = req.params;
-  const updateUser = req.body;
+//Let user add movies to their fasvorite list
+app.post('/users/:UserName/movies/:MovieID', (req, res) => {
+  Users.findOneAndUpdate({ UserName: req.params.UserName }, {
+     $push: { FavoriteMovies: req.params.MovieID }
+   },
+   { new: true },)
+   .then(updatedUser => {
+     res.json(updatedUser);
+   })
+   .catch(err => {
+     console.error(err);
+     res.status(500).send('Error: ' + err);
+   });
+ });
 
-  let user = users.find( user => user.id == id );
+//Allow user to remove movie from their favorite list
+app.delete('/users/:UserName/movies/:MovieID', (req, res) => {
+  Users.findOneAndUpdate({ UserName: req.params.UserName }, {
+     $pull: { FavoriteMovies: req.params.MovieID }
+   },
+   { new: true })
+   .then(updatedUser => {
+     res.json(updatedUser);
+   })
+   .catch(err => {
+     console.error(err);
+     res.status(500).send('Error: ' + err);
+   });
+ });
 
-  if(user) {
-    user.name = updateUser.name;
-    res.status(200).json(user);
-  } else {
-    res.status(400).send('no such user')
-  }
-  
-})
-
-//CREATE
-app.post('/users/:id/:movieTitle', (req, res) => {
-  const { id, movieTitle } = req.params;
-
-  let user = users.find( user => user.id == id );
-
-  if(user) {
-    user.favoriteMovies.push(movieTitle);
-    res.status(200).json(user);
-  } else {
-    res.status(400).send('no such movie')
-  }
-  
-})
-
-//DELETE
-app.delete('/users/:id/:movieTitle', (req, res) => {
-  const { id, movieTitle } = req.params;
-
-  let user = users.find( user => user.id == id );
-
-  if(user) {
-    user.favoriteMovies = user.favoriteMovies.filter( title => title !== movieTitle);
-    res.status(200).send(`${movieTitle} has been removed from user ${id}'s array`);
-  } else {
-    res.status(400).send('no such movie')
-  }
-  
-})
-
-//DELETE
-app.delete('/users/:id', (req, res) => {
-  const { id } = req.params;
-
-  let user = users.find( user => user.id == id );
-
-  if(user) {
-    users = users.filter( user => user.id != id);
-    res.status(200).send(`user ${id} has been deleted!`);
-  } else {
-    res.status(400).send('no such user')
-  }
-  
-})
-
-
-// app.use(morgan('common'));
-
-// app.get('/', (req, res) => {
-//   res.send('Welcome to my Movie Page!');
-// });
-
-// app.get('/documentation', (req, res) => {                  
-//   res.sendFile('public/documentation.html', { root: __dirname });
-// });
-
-// app.get('/movies', (req, res) => {
-//   res.json(topMovies);
-// });
-
-// app.use((err, req, res, next) => {
-//     console.error(err.stack);
-//     res.status(500).send('Something broke!');
-//   });
-
-// const accessLogStream = fs.createWriteStream(
-//     path.join(__dirname, 'log.txt'), {flags: 'a'});
-
-// app.use(morgan('combined', {stream: accessLogStream}));
-  
-// app.get('/secreturl', (req, res) => {
-//     res.send('This is a secret url with super top-secret content.');
-//   });
+//allow user to deregister
+app.delete('/users/:UserName', (req, res) => {
+  Users.findOneAndRemove({ UserName: req.params.UserName })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.Username + ' was not found');
+      } else {
+        res.status(200).send(req.params.Username + ' was deleted.');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
   
 app.listen(8080, () => {
     console.log('Your app is listening on port 8080.');
   });
+
